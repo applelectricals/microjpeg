c9dd0ccdc5b084aba88d547b5045972a4ba0f5a3
Prasun <applelectricals@gmail.com>
2025-10-04 01:02:52 +0530

    Fix __dirname in emailService for ES modules

--- server/emailService.ts
+++ server/emailService.ts
@@ -1,3 +1,9 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import { MailService } from '@sendgrid/mail';
 import nodemailer from 'nodemailer';
 import crypto from 'crypto';
